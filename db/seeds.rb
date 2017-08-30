e41f7375af72c74118c5003b091156792032c2f0
Michael Fournier <4niermike@gmail.com>
2017-08-30 13:49:31 -0400

    Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,15 @@ posts = Post.all
   )
 end
 
+200.times do
+  Advertisement.create!(
+    title: RandomData.random_sentence,
+    copy: RandomData.random_paragraph,
+    price: rand(5..45)
+  )
+end
+
 puts "Seed Finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Advertisement.count} ads created"
