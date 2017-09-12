8779b3021d4594c3b84e4598716c29bdc45e1ec1
Michael Fournier <4niermike@gmail.com>
2017-09-12 14:05:40 -0400

    Add comment shallow nesting/view in post

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,6 +30,7 @@ posts = Post.all
 
 100.times do
   Comment.create!(
+    user: users.sample,
     post: posts.sample,
     body: RandomData.random_paragraph
   )
@@ -41,7 +42,7 @@ admin = User.create!(
    password: 'helloworld',
    role:     'admin'
  )
- 
+
  member = User.create!(
    name:     'Member User',
    email:    'member@example.com',
