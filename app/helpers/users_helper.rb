a2c5193ddfe99ddc7697f98a45021af124410a3e
Michael Fournier <4niermike@gmail.com>
2017-09-21 14:53:11 -0400

    completed

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,34 @@
 module UsersHelper
+
+  def no_posts?(user)
+    @posts = user.posts.count
+
+    if @posts == 0
+      true
+    else
+      false
+    end
+  end
+
+  def no_comments?(user)
+    @comments = user.comments.count
+
+    if @comments == 0
+      true
+    else
+      false
+    end
+  end
+
+  def no_favorites?(user)
+    @favorites = user.favorites.count
+
+    if @favorites == 0
+      true
+    else
+      false
+    end
+  end
+
+
 end
